a9174685599fb814b6868ba6ebd47f5086939ce6
Ambroise Maupate <ambroise@rezo-zero.com>
2021-10-25 13:59:17 +0200

    Allow php8

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test :
 	./vendor/bin/phpunit --bootstrap vendor/autoload.php tests/
 	./vendor/bin/phpcbf -p
-	./vendor/bin/phpstan analyse -c phpstan.neon -l max src
+	./vendor/bin/phpstan analyse -c phpstan.neon
--- src/Exception/EmailNotSupported.php
+++ src/Exception/EmailNotSupported.php
@@ -7,6 +7,11 @@ use Throwable;
 
 class EmailNotSupported extends \InvalidArgumentException
 {
+    /**
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
     final public function __construct($message = "", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
